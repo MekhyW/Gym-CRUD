165bea4ab707502cea2b1253e5c5480ecd3b3f6f
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-11-27 09:28:48 -0300

    Autorun db_script.sql on startup

--- database.py
+++ database.py
@@ -13,11 +13,16 @@ PASSWORD = os.getenv("PASSWORD")
 DB = os.getenv("DB")
 
 SQLALCHEMY_DATABASE_URL = f"mysql://{USER}:{PASSWORD}@{SERVER}/{DB}"
+sql_script_path = 'assets/db_script.sql'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 if not database_exists(SQLALCHEMY_DATABASE_URL):
     create_database(SQLALCHEMY_DATABASE_URL)
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    with open(sql_script_path, 'r') as f:
+        sql_script = f.read()
+    with engine.connect() as con:
+        con.execute(sql_script)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
